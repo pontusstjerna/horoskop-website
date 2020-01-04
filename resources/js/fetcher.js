de78ccc92a15b65f3de0757353f392f631be957b
Pontus Stjernström <pontus@wmyl.se>
2020-01-04 11:43:07 +0100

    begin with fetching

--- resources/js/fetcher.js
+++ resources/js/fetcher.js
@@ -1,1 +1,14 @@
-export default () => new Promise(resolve => setTimeout(() => resolve("Du borde knarka."), 1000));
+import fetch from "unfetch";
+const URL = 'http://192.168.0.61:3000';
+const wordCount = 100;
+
+export default () => fetch(`${URL}?wordCount=${wordCount}`, {
+    headers: [{
+        "Authorization": "Basic YXBpX2NvbnN1bWVyOjA5a3dlZmszMDlrZjA5b2tmOTBrZjM0Zms="
+    }],
+}).then(result => {
+    if (result.ok) {
+        return result.text();
+    }
+    return "Något gick fel. :( - " + result.status + " " + result.statusText;
+});
