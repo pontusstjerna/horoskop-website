305ca5ed247e0c1bd5a5790c3b65c3dc8b7269b2
Pontus Stjernström <pontus@wmyl.se>
2020-01-04 12:28:14 +0100

    add fetcher as polyfill i think

--- resources/js/fetcher.js
+++ resources/js/fetcher.js
@@ -3,9 +3,9 @@ const URL = 'http://192.168.0.61:3000';
 const wordCount = 100;
 
 export default () => fetch(`${URL}?wordCount=${wordCount}`, {
-    headers: [{
+    headers: {
         "Authorization": "Basic YXBpX2NvbnN1bWVyOjA5a3dlZmszMDlrZjA5b2tmOTBrZjM0Zms="
-    }],
+    },
 }).then(result => {
     if (result.ok) {
         return result.text();
