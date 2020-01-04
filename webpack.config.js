009010dec9b4d84e90cff5327e9d528413cddd4e
Pontus Stjernström <pontus@wmyl.se>
2020-01-04 10:19:00 +0100

    add babel transpiling to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+// Since this is the top config file it can't use all ES6 features :(
+
 const path = require('path');
 
 module.exports = {
     entry: './resources/js/app.js',
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'public/dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/, // this is the regex
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
